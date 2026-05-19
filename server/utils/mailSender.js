8dace4a11675f0afd01fd7ded5a379d9f6e9a80d
Utkarsh Raj <180431336+Utkarsh-Raj-Profile@users.noreply.github.com>
2026-05-20 00:39:53 +0530

    fix: optimize brevo smtp for render

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,25 +2,24 @@ const nodemailer = require("nodemailer")
 
 const mailSender = async (email, title, body) => {
   try {
-
     console.log("Sending OTP to:", email)
 
+    // Create transporter
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+      host: "smtp-relay.brevo.com",
       port: 587,
       secure: false,
+      requireTLS: true,
 
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
 
-      connectionTimeout: 10000,
+      connectionTimeout: 60000,
+      greetingTimeout: 30000,
+      socketTimeout: 60000,
     })
-
-    // Verify SMTP connection
-    await transporter.verify()
-    console.log("Mail server connected successfully")
 
     // Send Mail
     let info = await transporter.sendMail({
@@ -34,7 +33,6 @@ const mailSender = async (email, title, body) => {
     console.log("Message ID:", info.messageId)
 
     return info
-
   } catch (error) {
     console.log("Mail Error:", error.message)
     throw error
